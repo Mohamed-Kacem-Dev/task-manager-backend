c639b373c24a0cf84153587fa85db876ab486f4e
Mohamed KACEM <ham.kacem15@gmail.com>
2023-12-19 18:15:10 +0100

    ADDED README.MD

--- config/database.js
+++ config/database.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
+    const mongoURI = process.env.MONGODB_URI;
+
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
+    console.error("Error connecting to MongoDB:", error.message);
     process.exit(1); // Exit process with failure
   }
 };
